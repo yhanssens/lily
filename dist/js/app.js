4bdb760b6a5b39a29ed7fe6e317db193a1622e6b
Youri Hanssens <youri@23g.nl>
2020-05-06 17:47:43 +0200

    Test production assets

---
+++ dist/js/app.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p("bUC5"),n.exports=p("zWCp")},bUC5:function(n,o){},zWCp:function(n,o){}},[[0,1]]]);
+//# sourceMappingURL=app.js.map
