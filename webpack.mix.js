f4f7f81053c9e24420b57fb34b11aa8e650d54fd
Youri Hanssens <youri@23g.nl>
2020-05-06 14:47:20 +0200

    Init

---
+++ webpack.mix.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const mix = require('laravel-mix');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+Mix.paths.setRootPath(process.cwd());
+
+mix
+    .js('resources/js/app.js', 'dist/js')
+    .sass('resources/scss/app.scss', 'dist/css')
+    .extract()
+    .browserSync({
+        proxy: 'http://lily.test',
+        host: 'lily.test',
+        open: false,
+    })
+    /* .options({
+        extractVueStyles: true,
+        globalVueStyles: 'scss/global.scss',
+    }) */
+    .webpackConfig({
+        devtool: mix.inProduction() ? 'source-map' : 'eval-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    enforce: 'pre',
+                    exclude: /node_modules/,
+                    options: {
+                        formatter: require('eslint-friendly-formatter'),
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new StylelintPlugin({
+                files: ['**/*.?(vue|scss)'],
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js'),
+            },
+            extensions: ['.js', '.vue'],
+        },
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
